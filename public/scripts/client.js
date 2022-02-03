03005f485ad7ec0cd000dbedd5724b50547d07b5
Matheson94 <mathesonchisholm@hotmail.com>
2022-02-03 00:13:12 +0000

    Scroll up button functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -108,9 +108,26 @@ $(() => {
       $('form').trigger('reset');
     })
   });
-  
+
 // Toggles the new-tweet form 
   $('.newtweetnav').on('click', function() {
     $('.new-tweet').slideToggle(200);
   })
+
+  const btn = $('#button');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
 })
+
