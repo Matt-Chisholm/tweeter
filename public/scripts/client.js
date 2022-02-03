0d318deb20971457407c0747662033b043873607
Matheson94 <mathesonchisholm@hotmail.com>
2022-02-03 22:29:05 +0000

    code cleanup/comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,33 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
 // Document.ready
 $(() => {
   const escape = function (str) {
@@ -37,7 +10,7 @@ $(() => {
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
-// Dynamically create tweet element
+  // Dynamically create tweet element
   const createTweetElement = (tweet) => {
     let html = `<article>
           <header class="tweetheader">
@@ -57,32 +30,32 @@ $(() => {
           </footer>
         </article>`;
 
-    return html
+    return html;
   };
 
-// Render tweets from db
+  // Render tweets from db
   const renderTweets = (tweets) => {
     // console.log(tweets);
-      $(".tweetcontainer").empty();
-      for (let tweet of tweets) {
-        const $tweet = createTweetElement(tweet);
-        $(".tweetcontainer").prepend($tweet);
-      }
+    $(".tweetcontainer").empty();
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $(".tweetcontainer").prepend($tweet);
+    }
   };
   
-// Get method to retrieve tweets to send to render
+  // Get method to retrieve tweets to send to render
   const loadTweets = function() {
     $.ajax({
       url: "/tweets",
       method: "GET",
     }).then((data) => {
-        renderTweets(data);
-      })
+      renderTweets(data);
+    })
   };
 
   loadTweets();
 
-// AJAX POST request for form
+  // AJAX POST request for form
   $('form').on("submit", (event) => {
     event.preventDefault();
     const $text = $(".tweet-text").val();
@@ -108,22 +81,16 @@ $(() => {
       $('.new-tweet').slideToggle(200);
       // clears form after submit
       $('form').trigger('reset');
-    })
+    });
   });
 
-// Toggles the new-tweet form 
+  // Toggles the new-tweet form
   $('.newtweetnav').on('click', function() {
     $('.new-tweet').slideToggle(200);
     $('.newtweetnav').slideToggle(200);
   });
 
-  $(".fa-heart").bind('click', function(event) {
-    console.log('Clicked', this);
-    $(this).css("background-color", "pink");
-  });
-
-
-
+  // Scroll button function
   const btn = $('#button');
 
   $(window).scroll(function() {
@@ -139,5 +106,5 @@ $(() => {
     $('html, body').animate({scrollTop:0}, '300');
   });
 
-})
+});
 
